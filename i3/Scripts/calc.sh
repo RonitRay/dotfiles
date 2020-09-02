9964db852e3282f0fb5337f5ebf6c86b4330b8f1
RonitRay <ray.ronit@gmail.com>
2020-09-02 16:07:07 +0530

    added 2 scripts and modified i3 config

---
+++ i3/Scripts/calc.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Simplified and less universal version of 
+#   https://github.com/onespaceman/menu-calc
+# Main changes:
+# * removes logic for checking menu program and uses rofi directly
+# * removes redundant close option, Esc is enough
+# * makes it actually use the "clipboard" clipboard and not primary
+# only like 10 SLOC.
+
+menu="rofi -dmenu -location 1 -width 100 -lines 2"
+answer=$(echo "$@" | bc -l | sed '/\./ s/\.\{0,1\}0\{1,\}$//')
+
+action=$(echo -e "Copy to clipboard\nClear" |
+$menu -p "= $answer")
+
+case $action in
+    "Clear") $0 ;;
+    "Copy to clipboard") echo -n "$answer" | xclip -selection clipboard -r;;
+    "") ;;
+    *) $0 "$answer $action" ;;
+esac
---
+++ i3/Scripts/powermenu_dmenu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+choices="Logout\nShutdown\nRestart"
+
+chosen=$(echo -e "$choices" | rofi -i -dmenu -matching normal -location 1 -width 100 -lines 3)
+
+case "$chosen" in
+    Logout)
+        i3-msg exit ;;
+    Shutdown)
+        shutdown now ;;
+    Restart)
+        shutdown -r now ;;
+esac
